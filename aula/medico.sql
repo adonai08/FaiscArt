cd5460b50e8b5db36cd4a385f55c99da64f5a822
Adonai <aron1@aluno.ifal.edu.br>
2024-07-31 10:12:59 -0300

    aula

--- aula/medico.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO medico (crm, nome, especializacao) VALUES ("564654", "joao", "Dermatologista");
-
--- aula/update.sql
+++
@@ -1,3 +0,0 @@
-UPDATE ala SET nome = "Ala 6" WHERE id ="6";
-
-UPDATE medico SET especializacao = "cirurgia" WHERE crm ="1234";
