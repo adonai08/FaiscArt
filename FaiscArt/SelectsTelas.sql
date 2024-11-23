3071479223e560409ecabb0c62907560920ec9f3
Matheuss07 <matheusluna1925@gmail.com>
2024-11-23 16:45:32 -0300

    BD

--- FaiscArt/SelectsTelas.sql
+++ FaiscArt/SelectsTelas.sql
@@ -13,15 +13,17 @@ SELECT titulo AS TituloObra, imagem AS CaminhoImagem, Obra AS NomeObra
 FROM Publicacoes where FK_Usuario_ID = 1;
     
 #Buscar informações sobre a publicação "A Criação de Adão" e identificar quem a publicou.
-SELECT * FROM Publicacoes 
-WHERE titulo LIKE 'A Criação de Adão' OR Obra LIKE 'A Criação de Adão';
+SELECT u.nome AS NomeUsuario, p.titulo AS TituloPublicacao FROM Publicacoes p
+JOIN Usuario u ON p.FK_Usuario_ID = u.ID
+WHERE p.titulo = 'A Criação de Adão' OR p.Obra = 'A Criação de Adão';
 
 #Exibir uma lista de publicações que estão à venda e têm valor igual ou superior a 15.
 SELECT ID, titulo, valor, obra FROM Publicacoes WHERE valor >= 15;
 
 #Exibir informações de uma publicação específica feita por um usuário com ID = 1, cujo título seja CACHORROPORU.
-SELECT * 
-FROM Publicacoes WHERE FK_Usuario_ID = '1' AND titulo = 'CACHORROPORU';
+SELECT u.nome AS NomeUsuario, p.titulo AS TituloPublicacao, p.valor AS ValorPublicacao, p.Obra AS NomeObraFROM FROM Publicacoes p
+JOIN Usuario u ON p.FK_Usuario_ID = u.ID
+WHERE p.FK_Usuario_ID = '1' AND p.titulo = 'CACHORROPORU';
 
 #Obter os nomes dos usuários envolvidos em um chat que incluem o usuário chamado 'Matheus'.
 SELECT u1.nome AS NomeUsuario1, u2.nome AS NomeUsuario2 FROM Chat c
@@ -44,9 +46,9 @@ FROM Denuncias d
 JOIN Publicacoes p ON d.FK_Publicacoes_ID = p.ID
 WHERE d.FK_Usuario_ID = '3';
 
--- Buscar todas as publicações relacionadas a arte
-SELECT * FROM Publicacoes 
-WHERE titulo LIKE '%arte%' OR Obra LIKE '%arte%';
+-- Buscar todas as publicações relacionadas a desenho
+SELECT titulo AS Titulo, Obra AS NomeDaObra, valor AS Valor FROM Publicacoes 
+WHERE titulo LIKE '%desenho%' OR Obra LIKE '%desenho%';
 
 -- Selecionar a média dos valores das publicações
 SELECT AVG(valor) AS MediaValores FROM Publicacoes 
